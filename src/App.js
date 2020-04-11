ded2796b1cf0e191d9aa694729469aafdf7a4261
RED ANGPOW <red.angpow@gmail.com>
2020-04-11 19:02:28 +0800

    :y :W :
    :y
    :w
    
    :q
    :w
    :q

--- src/App.js
+++ src/App.js
@@ -1,54 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
-import data from './components/data/map.json'
-import mapboxgl from 'mapbox-gl';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiZXJoYW5mYWRsaSIsImEiOiJjanlpcXJ0d3EwZDF2M21xaHljZXcyN2x1In0.QykinU8klSV-ZCDEi0G8rQ';
-
+import React, { Component } from "react";
+import "./App.css";
+import data from "./components/data/map.json";
+import mapboxgl from "mapbox-gl";
+import { useState, useEffect } from "react";
+import axios from "axios";
+mapboxgl.accessToken = "pk.eyJ1IjoiZXJoYW5mYWRsaSIsImEiOiJjanlpcXJ0d3EwZDF2M21xaHljZXcyN2x1In0.QykinU8klSV-ZCDEi0G8rQ";
 
 class App extends Component {
-
- 
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-    lng: 5,
-    lat: 34,
-    zoom: 9
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            lng: 5,
+            lat: 34,
+            zoom: 9,
+        };
     }
 
     componentDidMount() {
-      const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/erhanfadli/ck84p63gd04gl1impbqwi9cza',
-      center: [101.90, 3.109308],
-      zoom: this.state.zoom
-      });
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: "mapbox://styles/erhanfadli/ck84p63gd04gl1impbqwi9cza",
+            center: [101.9, 3.109308],
+            zoom: this.state.zoom,
+        });
 
-      // alert(data.features[0].properties.latitude); Add Marker to map from JSON
-      data.features.forEach(element => {
-        console.log(element.properties.latitude)
-        var el = document.createElement('div');
-        el.className = 'marker';
-  
-    
-        new mapboxgl.Marker(el)
-          .setLngLat([element.properties.longitude, element.properties.latitude])
-          .addTo(map);
-      });
+        axios
+            .get(`/api`, {})
+            .then((res) => {
+                const data = res.data.covidList;
+                console.log(data);
 
-      }
+                const markers = [];
+                data.forEach((element) => {
+                    console.log(element.location.coordinates);
+                    var el = document.createElement("div");
+                    el.className = "marker";
 
-      render() {
+                    new mapboxgl.Marker(el).setLngLat([element.location.coordinates[0], element.location.coordinates[1]]).addTo(map);
+                    //markers.push(marker);
+                });
+
+                this.setState({
+                    map,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    render() {
         return (
-        <div>
-        <div ref={el => this.mapContainer = el} className="mapContainer" />
-        </div>
-        )
-        }
+            <div>
+                <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
+            </div>
+        );
+    }
 }
 
 export default App;
-
